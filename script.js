d554fdf7fe88b31e6737da78c47fa79ab7d33d86
haipham4501 <hai.pm4501@gmail.com>
2021-03-18 18:49:20 +0700

    update func

---
+++ script.js
@@ -0,0 +1,30 @@
+function a() {
+  document.getElementById("myText").value += 1;
+}
+function b() {
+  document.getElementById("myText").value += 2;
+}
+function c() {
+  document.getElementById("myText").value += 3;
+}
+function d() {
+  document.getElementById("myText").value += 4;
+}
+function e() {
+  document.getElementById("myText").value += 5;
+}
+function f() {
+  document.getElementById("myText").value += 6;
+}
+function g() {
+  document.getElementById("myText").value += 7;
+}
+function h() {
+  document.getElementById("myText").value += 8;
+}
+function i() {
+  document.getElementById("myText").value += 9;
+}
+function j() {
+  document.getElementById("myText").value += 0;
+}
