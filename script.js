e048db7a6b7748a31c1f3b68ea15ca7e7d3399a0
haipham4501 <hai.pm4501@gmail.com>
2021-03-20 09:32:27 +0700

    up js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function fnc(v) {
   document.getElementById("myText").value += v;
+  console.log (v);
 }
 
 function equal(){
   let x = document.getElementById("myText").value
   let y = eval(x)
   document.getElementById("myText").value = y
+  console.log (y);
 }
 
 var enter = document.getElementById("myText");
 enter.addEventListener("keyup",function(result){
   if (result.keyCode === 13) {
+      result.preventDefault();
       document.getElementById("eq").click();
   }
 })
@@ -18,3 +21,9 @@ enter.addEventListener("keyup",function(result){
 function cls() {
   document.getElementById("myText").value = ""
 }
+var del = document.getElementById("myText");
+enter.addEventListener("keyup",function(blank){
+  if (blank.keyCode === 46) {
+      document.getElementById("clr").click();
+  }
+})
